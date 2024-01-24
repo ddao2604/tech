67a9b6392c1a2ed3c86e5c37073496e4fd359a4f
dao tao <78058830+ddao2604@users.noreply.github.com>
2024-01-24 22:51:04 +0700

    Update ubtao.sh

--- ubtao.sh
+++ ubtao.sh
@@ -90,3 +90,32 @@ do
 
 done
 
+for i in {1..10}
+do 
+	sample=$(az account subscription list)
+	for row in $(echo "${sample}" | jq -r '.[] | @base64');
+	do
+	  	_jq() {
+			echo ${row} | base64 --decode | jq -r ${1}
+		}
+		subid="$(_jq '.subscriptionId')"
+	  	az account set --subscription "$subid"
+	  	declare -a arr=("EastUS" "NorthEurope" "WestEurope" "SoutheastAsia" "EastAsia" "WestUS" "JapanWest" "JapanEast" "EastUS2" "NorthCentralUS" "SouthCentralUS" "BrazilSouth" "AustraliaEast" "AustraliaSoutheast" "CentralUS" "CentralIndia" "SouthIndia" "CanadaCentral" "CanadaEast" "WestUS2" "UKWest" "UKSouth" "KoreaCentral" "FranceCentral" "SouthAfricaNorth" "SwitzerlandNorth" "GermanyWestCentral" "UAENorth" "NorwayEast" "WestUS3" "SwedenCentral" "PolandCentral" "ItalyNorth" "IsraelCentral")
+	  	for loca in "${arr[@]}"
+		do
+			echo "$loca"
+			wk=$(az appservice plan show -n "$loca"_pt_1 -g GEastUS --query sku.capacity --output tsv)
+			echo $wk
+			let "nwk=$wk+1"
+		
+			if [[ "$nwk" -lt 6 ]]; then
+				az appservice plan update -n "$loca"_pt_1 -g GEastUS --number-of-workers "$nwk"
+			else
+				arr=("${arr[@]/$loca}")
+			fi
+			
+		
+		done
+  	done
+   	sleep 5
+done
