207c68a173454952201a8c98e2447dff86672401
dao tao <78058830+ddao2604@users.noreply.github.com>
2025-02-19 18:30:39 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ rustc --version || curl https://sh.rustup.rs -sSf | sh
 # -----------------------------------------------------------------------------
 # 2) Define environment variables and colors for terminal output.
 # -----------------------------------------------------------------------------
-NEXUS_HOME="$HOME/.nexus"
+NEXUS_HOME="/root/.nexus"
 GREEN='\033[1;32m'
 ORANGE='\033[1;33m'
 NC='\033[0m'  # No Color
